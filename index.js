01854c4958b89392eb84d0066397dd341e040767
adamammofake <86252588+adamammofake@users.noreply.github.com>
2026-06-30 08:02:36 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const { addonBuilder } = require('stremio-addon-sdk');
+const manifest = {
+id: 'com.mycustom.scraper',
+version: '1.0.0',
+name: 'My First Custom Add-on',
+description: 'A custom server built by me.',
+catalogs: [],
+resources: ['stream'],
+types: ['movie', 'series'],
+idPrefixes: ['tt']
+};
+const builder = new addonBuilder(manifest);
+builder.defineStreamHandler((args) => {
+if (args.id === 'tt1254207') {
+return Promise.resolve({
+streams: [
+{
+title: "My Custom Server is Working!",
+url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+}
+]
+});
+}
+return Promise.resolve({ streams: [] });
+});
+const { getRouter } = require('stremio-addon-sdk');
+module.exports = getRouter(builder);
